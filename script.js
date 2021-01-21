8985f72c7e286b0aaaa7b74ca9bbcd747609c826
Leslie den Adel <99062688@mydavinci.nl>
2021-01-21 14:39:26 +0100

    geluid toegevoegd

--- script.js
+++ script.js
@@ -2,42 +2,65 @@ var guessWord = words[Math.floor(Math.random() * words.length)];
 var saveWord = [...guessWord.split("")]
 guessWord = [...guessWord.split("")]
 var guess = 1;
+win = false;
+
+createBoxes();
+
+function createBoxes(){
+    //creates boxes to put letters in.
+    if (!guessWord.every(element => element === '')&&guess<6){
+        var wordholder = document.createElement('ul')
+        wordholder.id = "guess " + guess
+        document.getElementById('wordlist').appendChild(wordholder)
+        for (i=0;i<5;i++){
+            var box = document.createElement('li');
+            box.id = i
+            document.getElementById("guess " + guess).appendChild(box)
+        }
+        document.getElementById(0).innerHTML = saveWord[0]
+        document.getElementById(0).style.backgroundColor = "green"    
+    }
+    
+}
 
 function check() {
-    //creates boxes to put letters in
-    var wordholder = document.createElement('ul')
-    wordholder.id = "guess " + guess
-    document.getElementById('wordlist').appendChild(wordholder)
-    for (i=0;i<5;i++){
-        var box = document.createElement('li');
-        box.id = i
-        document.getElementById("guess " + guess).appendChild(box)
-    }
-
     //checks word
-    var inputWord = document.getElementById("inputWord").value
-    inputWord = inputWord.split("")
-    for (i = 0; i < guessWord.length; i++) {
-        document.getElementById(i).innerHTML = inputWord[i];
-        document.getElementById(i).style.backgroundColor = "white"
-        if (guessWord[i] == inputWord[i]) {
-            document.getElementById(i).style.backgroundColor = "green"
-            guessWord[i] = ""
-            inputWord[i] = ""
-        }
-    }
-    for (a = 0; a < guessWord.length; a++) {
-        for (b = 0; b < inputWord.length; b++) {
-            if (guessWord[a] != "" && guessWord[a] == inputWord[b]) {
-                document.getElementById(b).style.backgroundColor = "yellow"
-                guessWord[a] = ""
-                inputWord[b] = ""
+    if (!guessWord.every(element => element === '')&&guess<6){
+        guessWord = [...saveWord]
+        var inputWord = document.getElementById("inputWord").value
+        inputWord = inputWord.split("")
+        for (i = 0; i < guessWord.length; i++) {
+            document.getElementById(i).innerHTML = inputWord[i];
+            document.getElementById(i).style.backgroundColor = "white"
+            if (guessWord[i] == inputWord[i]) {
+                document.getElementById(i).style.backgroundColor = "green"
+                guessWord[i] = ""
+                inputWord[i] = ""
             }
         }
-    }
-    for (i=0;i<5;i++){
-        document.getElementById(i).id = ""
-    }
-    guessWord = [...saveWord]
+        for (a = 0; a < guessWord.length; a++) {
+            for (b = 0; b < inputWord.length; b++) {
+                if (guessWord[a] != "" && guessWord[a] == inputWord[b]) {
+                    document.getElementById(b).style.backgroundColor = "yellow"
+                    guessWord[a] = ""
+                    inputWord[b] = ""
+                }
+            }
+        }
+        for (i=0;i<5;i++){
+            document.getElementById(i).id = ""
+        }
     guess++
+    createBoxes();
+    if (guessWord.every(element => element === '')&&guess<6){
+        let audio = new Audio('sounds/noice.mp3');
+        audio.play();
+        setTimeout(function(){location.reload();},3000)
+    }
+    }
+    if (guess>5){
+        let audio = new Audio('sounds/mission failed.mp3');
+        audio.play();
+        setTimeout(function(){location.reload();},2000)
+    }
 }
